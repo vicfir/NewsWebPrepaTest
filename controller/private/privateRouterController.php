c8286cf82543c33857f5d33be9d029cf42fece36
vicfir <vicfir@hotmail.fr>
2022-05-18 16:20:36 +0200

    adminview twig v1

--- controller/private/privateRouterController.php
+++ controller/private/privateRouterController.php
@@ -7,7 +7,7 @@ if (isset($_GET["disconnect"]) || $_SESSION["idSession"] !== session_id()) {
     header("Location: ./");
     die();
 } else {
-    echo "<h1>You are logged in!</h1><pre>";
+    echo $twig->render('private/homepage.html.twig');
     var_dump($_SESSION);
     echo "</pre><p><a href='?disconnect'>Logout?</a></p>";
     // ici l'appel de la vue Twig (private homepage)
